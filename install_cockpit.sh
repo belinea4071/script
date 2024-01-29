9ac0b23c19164dd6545e61b7204e1a8cc98c5a4a
Guido Eberle <51459140+belinea4071@users.noreply.github.com>
2024-01-29 23:52:34 +0100

    Update install_cockpit.sh

--- install_cockpit.sh
+++ install_cockpit.sh
@@ -25,17 +25,17 @@ apt install cockpit-navigator -y
 # Install cockpit-identities
 apt install cockpit-identities -y
 
+# Add dummy network interface
+nmcli con add type dummy con-name fake ifname fake0 ip4 1.2.3.4/24 gw4 1.2.3.1
+
 # Install cockpit-zfs-manager
 apt install cockpit-zfs-manager -y
 
 # Install additional Cockpit packages
 apt-get install -y cockpit cockpit-packagekit cockpit-pcp cockpit-storaged tuned
 
-# Add dummy network interface
-nmcli con add type dummy con-name fake ifname fake0 ip4 1.2.3.4/24 gw4 1.2.3.1
-
-# comment entry for root in /etc/cockpit/disallowed-users
-sed -i 's/root/#root/' /etc/cockpit/disallowed-users
+# Uncomment entry for root in /etc/cockpit/disallowed-users
+sed -i 's/#root/root/' /etc/cockpit/disallowed-users
 
 # Get server IP address
 server_ip=$(hostname -I | awk '{print $1}')
@@ -43,7 +43,23 @@ server_ip=$(hostname -I | awk '{print $1}')
 # Restart Cockpit service
 systemctl restart cockpit.service
 
-# EOF text with dynamic server IP
+# Create a new user "eberle" with Samba permissions
+useradd -m eberle
+echo "Set a password for the user 'eberle':"
+passwd eberle
+
+# Install Samba if not installed
+apt-get install -y samba
+
+# Add "eberle" to Samba
+smbpasswd -a eberle
+
+# Create a new sudo user "gueb"
+useradd -m -G sudo gueb
+echo "Set a password for the user 'gueb':"
+passwd gueb
+
+# Display information
 cat <<EOF
 
 *************************************************
@@ -56,5 +72,9 @@ Cockpit has been successfully installed and configured.
 The entry for root in /etc/cockpit/disallowed-users has been uncommented.
 You can access Cockpit by navigating to http://$server_ip:9090 in your web browser.
 
-EOF
+User 'eberle' has been created with Samba permissions.
+Make sure to remember the password you set for 'eberle'.
 
+A new sudo user 'gueb' has been created.
+
+EOF
